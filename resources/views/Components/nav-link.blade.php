80c32c289f16b11e28649cd2cd3edfde191474a8
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-11-21 18:47:03 -0600

    Add support for button type in nav-link component

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,8 +1,20 @@
-@props(['active' => false])
-<a
-  {{ $attributes }}
-  class="{{ $active ? 'bg-gray-100 dark:bg-zinc-950' : '' }} block rounded-lg px-4 py-2 text-sm font-medium hover:bg-gray-100 dark:hover:bg-zinc-800"
-  aria-current="{{ $active ? 'page' : 'false' }}"
->
-  {{ $slot }}
-</a>
+@props([
+    'active' => false,
+    'type' => 'a',
+])
+
+@if ($type == 'a')
+  <a
+    {{ $attributes }}
+    class="{{ $active ? 'bg-gray-100 dark:bg-zinc-950' : '' }} block rounded-lg px-4 py-2 text-sm font-medium hover:bg-gray-100 dark:hover:bg-zinc-800"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+  >
+    {{ $slot }}
+  </a>
+@elseif ($type == 'button')
+  <button
+    class="{{ $active ? 'bg-gray-100 dark:bg-zinc-950' : '' }} block rounded-lg px-4 py-2 text-sm font-medium hover:bg-gray-100 dark:hover:bg-zinc-800"
+  >
+    {{ $slot }}
+  </button>
+@endif
