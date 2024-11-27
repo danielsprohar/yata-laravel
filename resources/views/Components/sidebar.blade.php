5a38ed87c6a84062eb2f1a700b44742727c060e5
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-11-27 16:15:48 -0600

    feat: add sidebar component with navigation links and logout functionality

---
+++ resources/views/Components/sidebar.blade.php
@@ -0,0 +1,112 @@
+@props(['workspaces'])
+
+{{-- Sidenav --}}
+<div
+  class="flex h-screen w-60 flex-col justify-between border-e bg-white dark:border-zinc-800 dark:bg-zinc-900"
+>
+  <div class="px-2 py-4">
+    <ul class="space-y-1">
+      <li>
+        <x-nav-link href="/profile">
+          <div class="flex items-center gap-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            {{ Auth::user()->name }}
+          </div>
+        </x-nav-link>
+      </li>
+      <li>
+        <x-nav-link
+          href="/"
+          :active="request()->is('/')"
+        >
+          <div class="flex items-center gap-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z"
+              />
+            </svg>
+            Inbox
+          </div>
+        </x-nav-link>
+      </li>
+
+      <li>
+        <x-nav-link
+          href="/workspaces"
+          :active="request()->is('workspaces')"
+        >
+          <div class="flex items-center gap-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3"
+              />
+            </svg>
+            Workspaces
+          </div>
+        </x-nav-link>
+      </li>
+    </ul>
+  </div>
+
+  <div class="sticky inset-x-0 bottom-0 p-2">
+    <form
+      action="/logout"
+      method="post"
+    >
+      @csrf
+      <x-nav-link
+        type="button"
+        href="/logout"
+      >
+        <div class="flex items-center gap-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+            />
+          </svg>
+          <span>Sign out</span>
+        </div>
+      </x-nav-link>
+    </form>
+  </div>
+</div>
