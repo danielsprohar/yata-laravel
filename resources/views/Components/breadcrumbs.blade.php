0ad9cba1221feac41e7f801105122590ebf3b9c4
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-11-28 07:36:11 -0600

    Create a breadcrumbs component

---
+++ resources/views/Components/breadcrumbs.blade.php
@@ -0,0 +1,70 @@
+<nav aria-label="Breadcrumb">
+  <ol class="flex items-center gap-1 text-sm text-gray-600 dark:text-gray-300">
+    <li class="rtl:rotate-180">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m9 20.247 6-16.5"
+        />
+      </svg>
+    </li>
+
+    <li>
+      <a
+        href="/workspaces"
+        class="block transition hover:text-zinc-700 hover:underline dark:hover:text-zinc-200"
+      >
+        Workspaces
+      </a>
+    </li>
+
+    <li class="rtl:rotate-180">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m9 20.247 6-16.5"
+        />
+      </svg>
+    </li>
+
+    <li>
+      {{ $workspace->name }}
+    </li>
+
+    <li class="rtl:rotate-180">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m9 20.247 6-16.5"
+        />
+      </svg>
+    </li>
+    <li>
+      Projects
+    </li>
+  </ol>
+</nav>
--- resources/views/workspaces/show.blade.php
+++ resources/views/workspaces/show.blade.php
@@ -1,80 +1,9 @@
 <x-app-layout>
   <div class="container">
-    <nav aria-label="Breadcrumb">
-      <ol
-        class="flex items-center gap-1 text-sm text-gray-600 dark:text-gray-300"
-      >
-        <li class="rtl:rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-4"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m9 20.247 6-16.5"
-            />
-          </svg>
-        </li>
-
-        <li>
-          <a
-            href="/workspaces"
-            class="block transition hover:text-zinc-700 hover:underline dark:hover:text-zinc-200"
-          >
-            Workspaces
-          </a>
-        </li>
-
-        <li class="rtl:rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-4"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m9 20.247 6-16.5"
-            />
-          </svg>
-        </li>
-
-        <li>
-          {{ $workspace->name }}
-        </li>
-
-        <li class="rtl:rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-4"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m9 20.247 6-16.5"
-            />
-          </svg>
-        </li>
-        <li>
-          Projects
-        </li>
-      </ol>
-    </nav>
-
+    <h1 class="text-2xl font-bold">{{ $workspace->name }}</h1>
+    <hr />
     <div class="flex items-center justify-between">
-      <h1 class="text-2xl font-bold">My Projects</h1>
+      <h2 class="text-xl font-bold">My Projects</h2>
       <a
         class="btn-primary"
         href="/workspaces/{{ $workspace->id }}/create/project"
